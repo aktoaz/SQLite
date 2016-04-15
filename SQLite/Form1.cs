5c301aa63136ba61698267a029032c83f596ea99
Paul Johnson <psj@mail.maricopa.gov>
2016-04-15 12:46:33 -0700

    Installed NuGet SQLite package and initial code

---
+++ SQLite/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //create table and insert
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+            string sql = "create table highscores (name varchar(20), score int)";
+
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('Me', 9001)";
+
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            m_dbConnection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //read first row in table
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+            using (m_dbConnection)
+            {
+                SQLiteDataAdapter sda = new SQLiteDataAdapter("select * from highscores", m_dbConnection);
+                SQLiteDataReader dr = sda.SelectCommand.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    label1.Text = dr["name"].ToString();
+                }
+            }
+        }
+    }
+}
